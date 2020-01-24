ecdd54db665cfb6119d4a35e25913bb65d09934a
yadavrg <y.ram119@gmail.com>
2020-01-24 19:44:48 +0530

    Version 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const web = require('./routes/web');
+
+const app = express();
+
+hbs.registerPartials("./views/partials");
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static("./public"));
+app.use('/', web);
+
+app.listen(1337, () => {
+	console.log('serving on port 1337');
+});
